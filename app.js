8c363d0b0d2a761ce32d2d69850de72735bc4555
Retno Ayu Adiati <retno_ayu@hotmail.com>
2020-07-27 19:13:44 +0200

    Add dashboard.pug
    Rename index.pug to login.pug
    
    Change hset to hmset
    Redis v3 cannot take more than 3 args
    Hmset still can be used for more than 3 args for this version
    
    Add function of saveSessionAndRenderDashboard
    
    Change cookie maxAge from 10 hours to 1 minute
    
    Add login handle

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ const RedisStore = require('connect-redis')(session)
 const client = redis.createClient()
 const port = process.env.port || 3000
 
+client.on('connect', () => {
+	console.log('Connected to Redis...')
+})
+
 // set up pug
 app.set('view engine', 'pug')
 app.set('views', path.join(__dirname, 'views'))
@@ -24,7 +28,7 @@ app.use(
 		resave: true,
 		saveUninitialized: true,
 		cookie: {
-			maxAge: 36000000, //10 hours, in milliseconds
+			maxAge: 60000, //1 minute, in milliseconds
 			httpOnly: false,
 			secure: false,
 		},
@@ -32,9 +36,13 @@ app.use(
 	})
 );
 
-
-
-app.get('/', (req, res) => res.render('index'))
+app.get('/', (req, res) => {
+	if(req.session.userId) {
+		res.render('dashboard')
+	} else {
+		res.render('login')
+	}
+})
 
 // receive login and password
 app.post('/', (req, res) => {
@@ -45,29 +53,50 @@ app.post('/', (req, res) => {
       message: 'Please provide both username and password'
     })
     return
-  }
+	}
 
-  client.hget('users', username, (err, userId) => {
-		if (!userId) {
-			// user does not exist, signup procedure
+	console.log(req.body, username, password);
+	
+	const saveSessionAndRenderDashboard = userId => {
+		req.session.userId = userId
+		req.session.save()
+		res.render('dashboard')
+	}
+
+	client.hget('users', username, (err, userId) => {
+		console.log('users from hget', username, userId)
+		if(!userId) {
+			// signup
 			client.incr('userId', async (err, userId) => {
-        client.hset('users', username, userId)
+				// create new user - signup
+				client.hset('users', username, userId)
 
-        // create password hash
-        const saltRounds = 10
-        const hash = await bcrypt.hash(password, saltRounds)
+				// hash password
+				const saltRounds = 10
+				const hash = await bcrypt.hash(password, saltRounds)
 
-        client.hset(`user:${userId}`, 'hash', hash, 'username', username)
-			});
+			console.log(`user:${userId}`, username, hash);
+
+			client.hmset(`user:${userId}`, 'hash', hash, 'username', username)
+			saveSessionAndRenderDashboard(userId)
+			})
 		} else {
-			// user exists, login procedure
+			// login
+			client.hget(`user:${userId}`, 'hash', async (err, hash) => {
+				const result = await bcrypt.compare(password, hash)
+				if(result) {
+					// password ok
+					saveSessionAndRenderDashboard(userId)
+				} else {
+					// wrong password
+					res.render('error', {
+						message: 'Incorrect password',
+					});
+					return;
+				}
+			})
 		}
-	});
-
-  console.log(req.body, username, password)
-  res.end()
+	})
 })
-
-
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
